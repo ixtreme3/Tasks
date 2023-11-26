44db47c906b0f38291d85afc2f2e326fb7228edc
Ivan Nemtsev <ixtreme3@yandex.ru>
2023-11-26 14:46:10 +0700

    Add CardView

--- app/src/main/java/com/example/tasks/TaskItemAdapter.kt
+++ app/src/main/java/com/example/tasks/TaskItemAdapter.kt
@@ -3,7 +3,9 @@ package com.example.tasks
 import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.CheckBox
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.tasks.db.entity.Task
 
@@ -17,32 +19,28 @@ class TaskItemAdapter: RecyclerView.Adapter<TaskItemAdapter.TaskItemViewHolder>(
 
     override fun getItemCount() = data.size
 
-    // Метод создающий (пока незаполненные) оболочки ViewHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemViewHolder {
         return TaskItemViewHolder.inflateFrom(parent)
     }
 
-    // Метод наполняющий (или обновляющий) оболочки ViewHolder посредством передачи определенного
-    // объекта данных в метод bind() объекта класса ViewHolder
     override fun onBindViewHolder(holder: TaskItemViewHolder, position: Int) {
         val item = data[position]
         holder.bind(item)
     }
 
-    // Класс оболочка над макетом, который умеет 1) надувать макет и хранить его внутри себя в виде
-    // надутого View; 2) заполнять надутый view данными с помощью функции bind()
-    class TaskItemViewHolder(private val rootView: TextView) : RecyclerView.ViewHolder(rootView) {
+    class TaskItemViewHolder(private val rootView: CardView) : RecyclerView.ViewHolder(rootView) {
+        private val taskName = rootView.findViewById<TextView>(R.id.task_name)
+        private val taskDone = rootView.findViewById<CheckBox>(R.id.task_done)
 
         fun bind(item: Task) {
-            rootView.text = item.taskName
+            taskName.text = item.taskName
+            taskDone.isChecked = item.taskDone
         }
 
-        // Способ получения view может отличаться (в зависимости от того используем binding-классы
-        // или нет)
         companion object {
             fun inflateFrom(parent: ViewGroup): TaskItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as TextView
+                val view = layoutInflater.inflate(R.layout.task_item, parent, false) as CardView
                 return TaskItemViewHolder(view)
             }
         }
